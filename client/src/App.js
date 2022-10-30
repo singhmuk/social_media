8f789f4494403c54090e8cd9fc51840d600b726e
mukesh <mukese0794@gmail.com>
2022-10-31 02:16:16 +0530

    review

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,33 @@
-import React from 'react'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
-import { Container } from 'react-bootstrap'
-import HomeScreen from './components/HomeScreen'
-import Counts from './components/counts'
-import Distincts from './components/distincts'
-import Projections from './components/projections'
-import Aggregations from './components/aggregations'
-import IndexCount from './components/indexCount'
-const App = () => {
-  return (
-    <Router>
-      <main className='py-3'>
-        <Container>
-          <Route exact path='/' component={HomeScreen} />
-          <Route path='/counts' component={Counts} />
-          <Route path='/distincts' component={Distincts} />
-          <Route path='/projections' component={Projections} />
-          <Route path='/aggregations' component={Aggregations} />
-          <Route path='/indexCount' component={IndexCount} />
+import React, { Component } from 'react';
+import { BrowserRouter, Route, Link } from 'react-router-dom'
+import { Button, Container } from 'reactstrap';
+import List from './components/List'
+import New from './components/New'
+import Review from './components/Review'
+import Edit from './components/Edit'
+import ReviewForm from './components/ReviewForm'
+
+class App extends Component {
+  render(){
+    return (
+      <BrowserRouter>
+        <Container className="mt-3">
+          <h1 className="mb-3">Lets Eat!</h1>
+          <Button outline color="primary" className="mr-3">
+            <Link to="/">Home</Link>
+          </Button>
+          <Button outline color="primary">
+            <Link to="/new">New Restaurant</Link>
+          </Button>
+          <Route exact path="/" component={List}/>
+          <Route exact path="/new" component={New}/>
+          <Route exact path="/restaurants/:_id" component={Review}/>
+          <Route exact path="/restaurants/:_id/edit" component={Edit}/>
+          <Route exact path="/restaurants/:_id/review" component={ReviewForm}/>
         </Container>
-      </main>
-    </Router>
-  )
+      </BrowserRouter>
+    );
+  }
 }
 
-export default App
+export default App;
