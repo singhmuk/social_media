45189467a0f1c5bed2c4ea35d343cf4be845ec95
mukesh <mukese0794@gmail.com>
2022-10-31 18:49:05 +0530

    dynsmic filter

--- server/models/paginations.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
-
-const pageSchema = new Schema({
-    text: { type: String, required: true },
-    title: { type: String, required: true }
-});
-
-module.exports = mongoose.model('paginations', pageSchema);
